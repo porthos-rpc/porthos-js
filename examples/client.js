ab4e7d787f1956395f1dfac43a5b79f4b1b8b08c
Germano Fronza <germano@eventials.com>
2016-08-07 11:26:42 -0300

    Reject the promise when a timeout occurs

--- examples/client.js
+++ examples/client.js
@@ -4,6 +4,8 @@ function bootstrapClient(broker) {
     porthos.createClient(broker, 'UserService', 5000).then(function(client) {
         client.call('doSomething', 20).then(function(response) {
             console.log('Response: %s', response);
+        }).catch(function(error) {
+            console.log('Error', error);
         });
     })
 };
